25e4cdca46aa75e8737e67b1f3500b9a83966115
nordberg <nordbeerg@nordbergPC.(none)>
2012-05-24 22:59:02 +0400

    new input format and some fix in discreteMethod

--- app.js
+++ app.js
@@ -2,62 +2,89 @@ var express = require('express');
 var app = express.createServer();
 var discreteOutputMethod = require('./lib/discreteOutputMethod');
 
-/*app.get('*', function(req, res, next) {
-
-});
-
-app.listen(3000);*/
-
-/*var Term = require('./lib/term');
-var x1 = new Term('x1', function(x) {return 0.2});
-var x2 = new Term('x2', function(x) {return 0.3});
-console.log(x1);
-console.log(x1.membershipFunction(1));
-console.log(x2);
-console.log(x2.membershipFunction(1));*/
 var input = {
-	terms: [{
-		name: "middle_work_time",
-		b: 170,
-		c: 170
+	linguisticVariables: [{
+		name: 'work time',
+		terms: [{
+			name: 'middle',
+			b: 170,
+			c: 170
+		}, {
+			name: 'low',
+			b: 150,
+			c: 150
+		}]
 	}, {
-		name: "low_work_time",
-		b: 150,
-		c: 150
+		name: 'cost',
+		terms: [{
+			name: 'high',
+			b: 38000,
+			c: 38000
+		}, {
+			name: 'middle',
+			b: 35000,
+			c: 35000
+		}]
 	}, {
-		name: "high_cost",
-		b: 38000,
-		c: 38000
+		name: 'quality',
+		terms: [{
+			name: 'middle',
+			b: 3,
+			c: 3
+		}, {
+			name: 'high',
+			b: 5,
+			c: 5
+		}]
 	}, {
-		name: "middle_cost",
-		b: 35000,
-		c: 35000
-	}, {
-		name: "middle_quality",
-		b: 3,
-		c: 3
-	}, {
-		name: "hight_quality",
-		b: 5,
-		c: 5
+		name: 'premium',
+		terms: [{
+			name: 'low'
+		}, {
+			name: 'middle'
+		}, {
+			name: 'high'
+		}]
 	}],
 	knowledgeBase: [{
-		term: 'low_premium',
+		output: {
+			linguisticVariable: 'premium',
+			term: 'low'
+		},
 		rules: [
-			[{term: "low_work_time", value: 170}, {term: "high_cost", value: 35000}, {term: "middle_quality", value: 3}]
+			[
+				{linguisticVariable: 'work time', term: 'low'},
+				{linguisticVariable: 'cost', term: 'high'},
+				{linguisticVariable: 'quality', term: 'middle'}
+			]
 		]
 	}, {
-		term: 'middle_premium',
+		output: {
+			linguisticVariable: 'premium',
+			term: 'middle'
+		},
 		rules: [
-			[{term: "middle_work_time", value: 170}, {term: "high_cost", value: 35000}, {term: "hight_quality", value: 5}],
+			[
+				{linguisticVariable: 'work time', term: 'middle'},
+				{linguisticVariable: 'cost', term: 'high'},
+				{linguisticVariable: 'quality', term: 'high'}
+			]
 		]
 	}, {
-		term: 'high_premium',
+		output: {
+			linguisticVariable: 'premium',
+			term: 'high'
+		},
 		rules: [
-			[{term: "low_work_time", value: 170}, {term: "middle_cost", value: 35000}, {term: "hight_quality", value: 5}]
+			[
+				{linguisticVariable: 'work time', term: 'low'},
+				{linguisticVariable: 'cost', term: 'middle'},
+				{linguisticVariable: 'quality', term: 'high'}
+			]
 		]
-	}]
+	}],
+	values: [{linguisticVariable: 'work time', value: 170}, {linguisticVariable: 'cost', value: 35000}, {linguisticVariable: 'quality', value: 3}]
 };
 //console.log(input);
-discreteOutputMethod.makeDecision(input);
+console.log(discreteOutputMethod.makeDecision(input));
 //console.log(discreteOutputMethod);
