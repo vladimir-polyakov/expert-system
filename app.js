03ecd001a66c388c9b6c5302046c90bee45a7911
nordberg <nordbeerg@nordbergPC.(none)>
2012-05-25 20:42:13 +0400

    new range method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express.createServer();
 var discreteOutputMethod = require('./lib/discreteOutputMethod');
+var continuosOutputMethod = require('./lib/continuosOutputMethod');
 
 app.use(express.bodyParser());
 
@@ -17,90 +18,6 @@ app.set('view options', {
 
 app.use(express.static(__dirname + '/static'));
 
-/*var input = {
-	linguisticVariables: [{
-		name: 'work time',
-		terms: [{
-			name: 'middle',
-			b: 170,
-			c: 170
-		}, {
-			name: 'low',
-			b: 150,
-			c: 150
-		}]
-	}, {
-		name: 'cost',
-		terms: [{
-			name: 'high',
-			b: 38000,
-			c: 38000
-		}, {
-			name: 'middle',
-			b: 35000,
-			c: 35000
-		}]
-	}, {
-		name: 'quality',
-		terms: [{
-			name: 'middle',
-			b: 3,
-			c: 3
-		}, {
-			name: 'high',
-			b: 5,
-			c: 5
-		}]
-	}, {
-		name: 'premium',
-		terms: [{
-			name: 'low'
-		}, {
-			name: 'middle'
-		}, {
-			name: 'high'
-		}]
-	}],
-	knowledgeBase: [{
-		output: {
-			linguisticVariable: 'premium',
-			term: 'low'
-		},
-		rules: [
-			[
-				{linguisticVariable: 'work time', term: 'low'},
-				{linguisticVariable: 'cost', term: 'high'},
-				{linguisticVariable: 'quality', term: 'middle'}
-			]
-		]
-	}, {
-		output: {
-			linguisticVariable: 'premium',
-			term: 'middle'
-		},
-		rules: [
-			[
-				{linguisticVariable: 'work time', term: 'middle'},
-				{linguisticVariable: 'cost', term: 'high'},
-				{linguisticVariable: 'quality', term: 'high'}
-			]
-		]
-	}, {
-		output: {
-			linguisticVariable: 'premium',
-			term: 'high'
-		},
-		rules: [
-			[
-				{linguisticVariable: 'work time', term: 'low'},
-				{linguisticVariable: 'cost', term: 'middle'},
-				{linguisticVariable: 'quality', term: 'high'}
-			]
-		]
-	}],
-	values: [{linguisticVariable: 'work time', value: 170}, {linguisticVariable: 'cost', value: 35000}, {linguisticVariable: 'quality', value: 3}]
-};
-
-console.log(discreteOutputMethod.makeDecision(input));*/
-
+/*console.log(discreteOutputMethod.makeDecision(require('./test/discreteOutputTest').input));
+console.log(continuosOutputMethod.makeDecision(require('./test/continuosOutputTest').input));*/
 app.listen(5000);
